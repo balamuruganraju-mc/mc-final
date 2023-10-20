7ff7a603a319be6770211708269a06ac4aa099e8
balamuruganraju-mc <147946734+balamuruganraju-mc@users.noreply.github.com>
2023-10-20 23:34:46 +0100

    weatherforecast project

---
+++ weatherforecast/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.Json;
+using simpleWebApp;
+
+namespace simpleWebApp.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public async void OnGet()
+    {
+        try
+            {            
+                HttpClient _httpClient = new HttpClient();
+                var request = new HttpRequestMessage();
+                request.RequestUri = new Uri("http://20.204.200.23:8080/WeatherForecast");
+                var response = _httpClient.Send( request );
+                string weatherForecastString = await response.Content.ReadAsStringAsync();
+                ViewData["pagedata"] = $"Response: {weatherForecastString}";
+                IList<WeatherForecast> WeatherForecastLists = JsonSerializer.Deserialize<IList<WeatherForecast>>(weatherForecastString);
+                ViewData["WeatherData"] = WeatherForecastLists;
+            }
+            catch
+            {
+                ViewData["pagedata"] = "Error occured";
+            }
+    }
+}
---
+++ weatherforecast/WeatherForecast.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace simpleWebApp
+{
+    public class WeatherForecast
+    {
+        [JsonPropertyName("date")]
+        public DateTime Date { get; set; }
+
+        [JsonPropertyName("temperatureC")]
+        public int TemperatureC { get; set; }
+
+        [JsonPropertyName("temperatureF")]
+        public int TemperatureF { get; set; }
+
+        [JsonPropertyName("summary")]
+        public string? Summary { get; set; }
+    }
+}
